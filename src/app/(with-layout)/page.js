98d4db681a9ab79febd5f6e913d89c4b7143a5cc
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2024-01-16 20:32:22 +0100

    first commit

---
+++ src/app/(with-layout)/page.js
@@ -0,0 +1,12 @@
+// "use client";
+
+const HomePage = () => {
+  // console.log("From server");
+  return (
+    <div>
+      <h1 className="text-6xl">This is Home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
